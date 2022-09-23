cb1214742308cc199899bdb94d11ac8f78a14473
Holmes-EH <holmes.samuel@yahoo.fr>
2022-09-23 11:42:06 +0200

    Update config.php
    Hide password even though the MySql server instance no longer exists...

--- api/config/config.php
+++ api/config/config.php
@@ -2,5 +2,5 @@
     //DB Params
     define('DB_HOST', 'localhost');
     define('DB_USER', 'interne-atalante');
-    define('DB_PASS', 'MopWr4E9L4hw738S');
+    define('DB_PASS', 'passwordhere');
     define('DB_NAME', 'interne_atalante');
